fe569691cad2da84c2a37dfe6cf5e207fca05856
Rohit Pednekar <rohitpednekar95@gmail.com>
2019-08-12 21:00:07 +0530

    removing of hardcoded dependencies

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var path = require('path');
 var bodyParser = require('body-parser');
+var constant = require('./constant');
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -8,7 +9,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-var server = app.listen( 5000, function() {
+var server = app.listen( constant.PORT, function() {
     console.log('Express server listening on port ' + server.address().port);
   });
 
